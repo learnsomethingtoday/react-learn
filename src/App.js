7a9bc28d5eacff71c81640c97fb984970c4c14a6
learnsomethingtoday <56990829+learnsomethingtoday@users.noreply.github.com>
2022-10-09 19:21:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+import ChangeText from './components/ChangeText'
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import EventBind from './components/EventBind';
+import FormHandling from './components/FormHandling';
+function App() {
+  return (
+    <div className="App">
+      {/* <Greet name="Shiva" />
+      <Greet name="Joan" />
+      <Greet name="Romeo" /> */}
+
+      {/* <ChangeText /> */}
+      {/* <Counter /> */}
+      {/* <FunctionClick /> */}
+      {/* <EventBind /> */}
+      <FormHandling />
+    </div>
+  );
+}
+
+export default App;
